aca6dc519a3a6dcf5475b2607b8aedf904dce618
Patriciavaldebenito <patricia.valdebenito@usach.cl>
2019-09-26 20:57:20 -0300

    Se agrega en componente pag-initial estilos css para font y text, se agrega estructura html

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PageInitialComponent } from './components/page-initial/page-initial.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"es-CL", component:PageInitialComponent},
+  {path:"", component:PageInitialComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PageInitialComponent } from './components/page-initial/page-initial.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageInitialComponent
   ],
   imports: [
     BrowserModule,
