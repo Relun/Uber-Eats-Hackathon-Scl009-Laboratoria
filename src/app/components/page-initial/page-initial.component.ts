92e53a510cd8281d1a755e011153e6391c711a32
Patriciavaldebenito <patricia.valdebenito@usach.cl>
2019-09-26 19:04:40 -0300

    creacion primer componente, pagina inicial, se agrega componente en const routes archivo de app-routing.module.ts

---
+++ src/app/components/page-initial/page-initial.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-page-initial',
+  templateUrl: './page-initial.component.html',
+  styleUrls: ['./page-initial.component.css']
+})
+export class PageInitialComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
